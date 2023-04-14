5733f627410922cee78fde9ede9169947ca9eee3
Pavel Bazin <pvlbzn@gmail.com>
2023-04-14 13:28:07 -0300

    add codebuild / codedeploy spec

---
+++ README.md
@@ -0,0 +1,3 @@
+# Deployment
+
+CodePipeline requires an S3 bucket to store artifacts built by CodeBuild.
--- deploy-infra.sh
+++ deploy-infra.sh
@@ -6,13 +6,31 @@ CLI_PROFILE=default
 
 EC2_INSTANCE_TYPE=t2.micro
 
+# Programmatically get the AWS account ID from the AWS CLI
+AWS_ACCOUNT_ID=`aws sts get-caller-identity --profile awsbootstrap \
+  --query "Account" --output text`
+CODEPIPELINE_BUCKET="$STACK_NAME-$REGION-codepipeline-$AWS_ACCOUNT_ID"
+
 # Deploy the CloudFormation template
 echo -e "\n\n=========== Deploying main.yml ==========="
+
+# Deploy S3
+aws cloudformation deploy \
+  --region $REGION \
+  --profile $CLI_PROFILE \
+  --stack-name $STACK_NAME-setup \
+  --template-file setup.yaml \
+  --no-fail-on-empty-changeset \
+  --capabilities CAPABILITY_NAMED_IAM \
+  --parameters-overrides \
+    CodePipelineBucket=$CODEPIPELINE_BUCKET
+
+# Deploy infra
 aws cloudformation deploy \
   --region $REGION \
   --profile $CLI_PROFILE \
   --stack-name $STACK_NAME \
-  --template-file main.yml \
+  --template-file main.yaml \
   --no-fail-on-empty-changeset \
   --capabilities CAPABILITY_NAMED_IAM \
   --parameter-overrides \
